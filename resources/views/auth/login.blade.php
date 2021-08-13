341a9759c0caa9a3198b210eba90856cc47c6686
Nguyen Duc Huy <huynd58@fpt.com.vn>
2021-08-13 22:13:52 +0700

    init chat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class=" fill-current text-gray-500" />
             </a>
         </x-slot>
 
@@ -24,7 +24,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Mật khẩu')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -43,12 +43,12 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Quên mật khẩu à :v ?') }}
                     </a>
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Đăng nhập') }}
                 </x-button>
             </div>
         </form>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,13 +1,18 @@
-@extends('layouts.master')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Trò chuyện nek') }}
+        </h2>
+    </x-slot>
 
-@section('title', 'Chat nek')
 
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar override.</p>
-@stop
-
-@section('content')
-    <p>This is my body content chees.</p>
-@stop
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Phòng chém gió
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
